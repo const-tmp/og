73efb0af098b3741948b2595ba57fe5fe4f7dda0
hightime <hightime@hightime.local>
2022-11-24 23:32:20 +0200

    protocol improved

--- pkg/templates/struct.go
+++ pkg/templates/struct.go
@@ -2,7 +2,8 @@ package templates
 
 var (
 	StructTemplate = `
-{{ .StructName }} struct {
+// {{ .StructName }} is an exchange struct
+type {{ .StructName }} struct {
 {{- range .Fields }}
 	{{ .Name }} {{ .Type.String }}
 {{- else -}}
@@ -11,25 +12,20 @@ var (
 `
 	ProtocolTemplate = `package {{ .Package }}
 
-type (
 {{ range .Structs }}
 {{ template "struct" . }}
-{{ end }}
-)
-
-{{ range .Structs }}
 {{ if .Fields }}
+
+// New{{ .StructName }} is a constructor for {{ .StructName }}
 func New{{ .StructName }} ({{ struct_constructor_args .Fields }}) {{ .StructName }} {
 	return {{ .StructName }}{ {{- struct_constructor_return .Fields -}} }
 }
-{{ end }}
-{{ end }}
 
-{{ range .Structs }}
-{{ if .Fields }}
+// Args is a shortcut method that returns args to original interface's method
 func (r {{ .StructName }}) Args(ctx context.Context) (context.Context, {{ struct_return_types .Fields }}) {
 	return ctx, {{ struct_return_args .Fields }}
 }
+
 {{ end }}
 {{ end }}
 `
